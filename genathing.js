9f4560f97ad13adfd20338a045d74846207648fb
dlyman545 <danlyman.3dartist@gmail.com>
2021-03-23 01:10:31 -0600

    Create genathing.js

---
+++ genathing.js
@@ -0,0 +1,18 @@
+var adj1 = [
+	'You\'re badass!',
+	'You can do it! All night long!',
+	'Better to remain silent and be thought a fool than to speak out and remove all doubt.',
+	'If I were two-faced, would I be wearing this one?',
+	'The best thing about the future is that it comes one day at a time.',
+	'The only mystery in life is why the kamikaze pilots wore helmets.',
+	'Light travels faster than sound. This is why some people appear bright until you hear them speak.',
+	'Nobody realizes that some people expend tremendous energy merely to be normal.',
+	'Men marry women with the hope they will never change. Women marry men with the hope they will change. Invariably they are both disappointed.',
+	'The difference between stupidity and genius is that genius has its limits.',
+	'The average dog is a nicer person than the average person.',
+]
+
+function genathingFN() {
+	var randNum = Math.floor(Math.random() * (adj1.length));
+	document.getElementById('posDisplay').innerHTML = adj1[randNum];
+}
